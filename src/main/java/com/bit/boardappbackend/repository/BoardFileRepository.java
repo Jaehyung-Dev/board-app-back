6ba6864c1295017c6c76df129fb1c4f25eeb5c2c
dev2024dev <publicdev2024@gmail.com>
2024-09-30 12:17:48 +0900

    1

--- src/main/java/com/bit/boardappbackend/repository/BoardFileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bit.boardappbackend.repository;
-
-import com.bit.boardappbackend.entity.BoardFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
-}
